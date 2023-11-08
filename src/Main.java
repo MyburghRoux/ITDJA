3aefb258a34399ececd03e3e5eb540a8a9b90c33
Myburgh Roux <myburghroux7@gmail.com>
2023-11-08 14:02:15 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Test code for Github!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("How many banks in the System?");
+        int n = scanner.nextInt();
+
+        System.out.println("Test 1");
+
     }
 }
